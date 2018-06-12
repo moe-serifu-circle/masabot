45aebe57e5377d433c763e610faa53f616ab65a4
Rebecca Nelson <dekarrin.irl@gmail.com>
2018-06-12 06:42:32 -0500

    Added new op commands in karma for changing buzzkill mode

--- masabot/commands/karma.py
+++ masabot/commands/karma.py
@@ -12,7 +12,11 @@ class KarmaModule(BotBehaviorModule):
 		help_text += " user or any item followed by a '++' or '--' to increase or decrease their karma. Add more"
 		help_text += " '+'/'-' characters to change the amount by even more.\n\nTo view the amount of karma, use the"
 		help_text += " `karma` command followed by the mention of the user to check (or the name of the thing to"
-		help_text += " check). As a shortcut, you can view your own karma by invoking `karma` with no arguments."
+		help_text += " check). As a shortcut, you can view your own karma by invoking `karma` with no arguments.\n\n"
+		help_text += "In order to prevent huge karma changes, there is a buzzkill mode, which limits the amount that"
+		help_text += " karma can change by. The `karma-buzzkill` command with no arguments will give what the current"
+		help_text += " buzzkill limit is, and ops are able to give an argument to set the limit. Setting the limit to"
+		help_text += " anything less than 1 disables buzzkill mode entirely, allowing any amount of karma change."
 
 		super().__init__(
 			bot_api,
@@ -21,7 +25,8 @@ class KarmaModule(BotBehaviorModule):
 			help_text=help_text,
 			triggers=[
 				RegexTrigger(r'<@!?(\d+)>\s*(\+\++|--+)$'),
-				InvocationTrigger('karma')
+				InvocationTrigger('karma'),
+				InvocationTrigger('karma-buzzkill')
 			],
 			has_state=True
 		)
@@ -37,14 +42,10 @@ class KarmaModule(BotBehaviorModule):
 		self._buzzkill_limit = state['buzzkill']
 
 	async def on_invocation(self, context, command, *args):
-		if len(args) >= 1:
-			m = re.search(r'<@!?(\d+)>$', args[0], re.DOTALL)
-			if m is None:
-				raise BotSyntaxError(str(args[0]) + " is not something that can have karma")
-			msg = self.get_user_karma(m.group(1))
-		else:
-			msg = self.get_user_karma(context.author.id)
-		await self.bot_api.reply(context, msg)
+		if command == "karma":
+			await self.show_karma(context, args)
+		elif command == "karma-buzzkill":
+			await self.configure_buzzkill(context, args)
 
 	async def on_regex_match(self, context, *match_groups):
 		msg = None
@@ -66,6 +67,42 @@ class KarmaModule(BotBehaviorModule):
 			else:
 				msg = self.add_user_karma(user, amount)
 		if msg is not None:
+			await self.bot_api.reply(context, msg)
+
+	async def show_karma(self, context, args):
+		if len(args) >= 1:
+			m = re.search(r'<@!?(\d+)>$', args[0], re.DOTALL)
+			if m is None:
+				raise BotSyntaxError(str(args[0]) + " is not something that can have karma")
+			msg = self.get_user_karma(m.group(1))
+		else:
+			msg = self.get_user_karma(context.author.id)
+		await self.bot_api.reply(context, msg)
+
+	async def configure_buzzkill(self, context, args):
+		if len(args) > 0:
+			self.bot_api.require_op(context, "attempted to set buzzkill level")
+			try:
+				new_limit = int(args[0])
+			except ValueError:
+				raise BotSyntaxError("I need the new limit to be an integer.")
+			if new_limit > 0:
+				msg = ""
+				if self._buzzkill_limit < 1:
+					msg += "Looks like buzzkill mode was disabled before. I'll turn it on now!\n\n"
+				self._buzzkill_limit = new_limit
+				msg += "All right, done! The most that karma can change by is now " + str(new_limit) + "."
+				await self.bot_api.reply(context, msg)
+			else:
+				self._buzzkill_limit = 0
+				msg = "Okay! Buzzkill mode has now been disabled. Karma change is now unlimited!"
+				await self.bot_api.reply(context, msg)
+		else:
+			if self._buzzkill_limit > 0:
+				msg = "Yep! Buzzkill mode is currently enabled; the most that karma can change by is currently"
+				msg += " " + str(self._buzzkill_limit) + "."
+			else:
+				msg = "Hmm... It looks like there is currently no limit to how much karma can change by. Hooray!"
 			await self.bot_api.reply(context, msg)
 
 	def get_user_karma(self, uuid):
