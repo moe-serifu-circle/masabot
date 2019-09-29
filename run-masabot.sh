53ce7332b7021e2161813133a49bceba9a79fbbc
Rebecca Nelson <dekarrin.irl@gmail.com>
2019-09-29 10:22:41 -0500

    Added new run script to specify venv

--- run-masabot.sh
+++ run-masabot.sh
@@ -1,22 +1,40 @@
 #!/bin/bash
 
 # starts up masabot and redeploys when necessary
+#
+# options:
+#
+# $1 - venv-dir - path to the directory to use for the virtual environment, default to '.venv' or 'venv' in that order
+# if none is given.
+
+if [ "$#" -ge 1 ]
+then
+  if [ -d "$1" ]
+  then
+    virtual_dir="$1"
+  else
+    echo "Virtual environemnt directory '$1' not found." >&2
+    echo "Abort." >&2
+    exit 1
+  fi
+else
+  echo "No virtual environment directory given; scanning for '.venv' or 'venv'..." >&2
+  if [ -d .venv ]
+  then
+      virtual_dir=.venv
+  elif [ -d venv ]
+  then
+      virtual_dir=venv
+  else
+      echo "Could not auto-detect virtual environment directory '.venv' or 'venv'." >&2
+      echo "Please create one for masabot before executing, or point to existing one with CLI arguments." >&2
+      exit 1
+  fi
+fi
 
 running=1
 
-virtual_dir=
-
-if [ -d .venv ]
-then
-    virtual_dir=.venv
-elif [ -d venv ]
-then
-    virtual_dir=venv
-else
-    echo "Virtual environment not found in '.venv' or 'venv'." >&2
-    echo "Please create one for masabot before executing." >&2
-    exit 1
-fi
+echo "Using virtual environment directory '$virtual_dir'"
 
 if [ -d "$virtual_dir/bin" ]
 then
