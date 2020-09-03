354a6a3e7b82e19d2748fc6ff5674cc624da2a3e
Rebecca Nelson <dekarrin.irl@gmail.com>
2020-09-02 22:20:47 -0500

    added notice me module

--- masabot/commands/noticeme.py
+++ masabot/commands/noticeme.py
@@ -4,12 +4,29 @@ from .. import util
 from .. import bot
 import discord
 
+import re
 import logging
 import random
 
 _log = logging.getLogger(__name__)
 _log.setLevel(logging.DEBUG)
 
+positive_words = [
+	"thanks",
+	"thank you",
+	"good bot",
+	"good girl",
+	"good job",
+	"thank",
+]
+
+negative_words = [
+	"fuck you",
+	"fuck off",
+	"bad bot",
+	"thanks for nothing",
+	"broken",
+]
 
 positive_responses = [
 	"Oh, thanks!",
@@ -20,8 +37,19 @@ positive_responses = [
 	"Um! I'm not sure I deserve that >///<",
 	"W-well I'm not sure that's necessary, but, um, thanks!",
 	"I'm so glad you think so!",
-	"I'm happy to help!",
-	"Haiiiiiiiii!"
+	"I'm always happy to help!",
+	"Haiiiiiiiii!",
+]
+
+negative_responses = [
+	"I'm sorry!",
+	"Oh, oh no... I didn't mean to!",
+	"Fuwawawaaaaaa @_@",
+	":("
+	"P-please, I didn't mean to be bad.",
+	"Oh no oh no oh no! I'm really sorry!",
+	"Oh.",
+	"Did you have to do that?"
 ]
 
 
@@ -55,11 +83,37 @@ class NoticeMeSenpaiModule(BotBehaviorModule):
 		await self._handle_mention(context, message.content)
 
 	async def _handle_mention(self, context: bot.BotContext, message_text: str):
-		if sentiment_is_good(message_text):
-			await self.bot_api.reply(context, "")
+		sentiment = analyze_sentiment(message_text)
+		if sentiment > 0:
+			await self.bot_api.reply(context, random.choice(positive_responses))
+		elif sentiment < 0:
+			await self.bot_api.reply(context, random.choice(negative_responses))
 
 
 def analyze_sentiment(message_text):
+	"""
+	Returns 1 for positive, 0 for neutral, -1 for negative.
+	:param message_text:
+	:return:
+	"""
+	all_words = {k.lower(): True for k in positive_words}
+	all_words.update({k.lower(): False for k in negative_words})
+	all_word_patterns = list(all_words.keys())
+	all_word_patterns.sort()
+
+	found = False
+	positive = False
+	for p in all_word_patterns:
+		m = re.search(r"\b" + p + "\b", message_text, re.IGNORECASE | re.MULTILINE)
+		if m:
+			found = True
+			positive = all_words[p]
+			break
+	if not found:
+		return 0
+	if positive:
+		return 1
+	return -1
 
 
 BOT_MODULE_CLASS = NoticeMeSenpaiModule
